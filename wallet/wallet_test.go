bec32e5a6360b5f6d70fa8f4634495a14f61b936
Bernhard Specht <bernhard@specht.net>
2018-06-16 10:11:28 +0200

    generalize test for getting block

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -49,32 +49,34 @@ func TestGetAccountsWithRewardRecipient(t *testing.T) {
 }
 
 func TestGetBlock(t *testing.T) {
-	res, err := w.GetBlock(4, 0, 0, true)
+	res, err := rw.GetBlock(471696, 0, 0, true)
 	if !assert.Nil(t, err) {
 		return
 	}
-	assert.Equal(t, "11a99766b8fe38cea2054ae3c1a0c33d94e2b0158525fb58a2e418ee7207658a",
-		res.PreviousBlockHash)
-	assert.Equal(t, 193, res.PayloadLength)
-	assert.Equal(t, int64(0), res.TotalAmountNQT)
-	assert.Equal(t, "26b90b5327fd4af633bdf235eccc530450b7d5b21ea84153da112b13d7ed2caf", res.GenerationSignature)
-	assert.Equal(t, uint64(13015131355133865118), res.Generator)
-	assert.Equal(t, "2e550bb843b6ce87ea76a585fe48e1e16be99c9d37d30838c0a89818614a6b5d", res.GeneratorPublicKey)
-	assert.Equal(t, uint64(18325193796), res.BaseTarget)
-	assert.Equal(t, "f358c2a533ba337ea94ed6c8b83c1b4fd1e698ae4c109b4c281227bd5c19a3c5", res.PayloadHash)
-	assert.Equal(t, "BURST-UV6Y-2CMW-QM7Y-DBS9B", res.GeneratorRS)
-	assert.Equal(t, int64(10000), res.BlockReward)
-	assert.Equal(t, uint64(11593430687155065378), res.NextBlock)
-	assert.Equal(t, uint32(3852), res.ScoopNum)
-	assert.Equal(t, "c0503a614c9ebacbb93a2187c4413ccb0f097ef5c9850b61ff319393fce1140b5a97c53a2892926bcee249b582bcf2e7ee67d96cf8e3aee7325d6b812f9557ef", res.BlockSignature)
-	assert.Equal(t, 1, len(res.Transactions))
-	assert.Equal(t, uint64(7830187391818421696), uint64(res.Transactions[0]))
-	assert.Equal(t, uint64(269511), res.Nonce)
-	assert.Equal(t, 3, res.Version)
-	assert.Equal(t, uint64(14859907038457604369), res.PreviousBlock)
-	assert.Equal(t, uint64(14996553267393132899), res.Block)
-	assert.Equal(t, uint64(4), res.Height)
-	assert.Equal(t, uint64(95200229), res.Timestamp)
+	assert.NotEmpty(t, res.PreviousBlockHash)
+	assert.NotEmpty(t, res.PayloadLength)
+	assert.NotEmpty(t, res.TotalAmountNQT)
+	assert.NotEmpty(t, res.GenerationSignature)
+	assert.NotEmpty(t, res.Generator)
+	assert.NotEmpty(t, res.GeneratorPublicKey)
+	assert.NotEmpty(t, res.BaseTarget)
+	assert.NotEmpty(t, res.PayloadHash)
+	assert.NotEmpty(t, res.GeneratorRS)
+	assert.NotEmpty(t, res.BlockReward)
+	assert.NotEmpty(t, res.NextBlock)
+	assert.NotEmpty(t, res.ScoopNum)
+	assert.NotEmpty(t, res.BlockSignature)
+	if assert.NotEmpty(t, len(res.Transactions)) {
+		assert.NotEmpty(t, uint64(res.Transactions[0]))
+	}
+	assert.NotEmpty(t, res.Nonce)
+	assert.NotEmpty(t, res.Version)
+	assert.NotEmpty(t, res.PreviousBlock)
+	assert.NotEmpty(t, res.Block)
+	assert.NotEmpty(t, res.Height)
+	assert.NotEmpty(t, res.Timestamp)
+}
+
 func TestGetAccountTransactions(t *testing.T) {
 	res, err := rw.GetAccountTransactions(1661865342978896789, 1, 0, 115714842)
 	if !assert.Nil(t, err) {
